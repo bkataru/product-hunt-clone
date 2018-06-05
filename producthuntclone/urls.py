28ce0b323350edd1d9c0005eae86fbbf1eb69c86
BK-Modding <kavesbteja@gmail.com>
2018-06-05 17:26:02 +0530

    site finished

--- producthuntclone/urls.py
+++ producthuntclone/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 import products.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', products.views.homepage, name="home"),
-    path('accounts/', include('accounts.urls'))
-]
+    path('accounts/', include('accounts.urls')),
+    path('products/', include('products.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
